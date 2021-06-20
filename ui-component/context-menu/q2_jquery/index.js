f8b0beabd201b4d492f8264306d4189bb85bb44c
mhkim <yahma25@gmail.com>
2021-06-20 21:26:43 +0900

    #1 UI 컴포넌트 - Context Menu - jQuery를 사용하여 구현하기 개선

--- ui-component/context-menu/q2_jquery/index.js
+++ ui-component/context-menu/q2_jquery/index.js
@@ -3,15 +3,35 @@ import "./style.css";
 import $ from "jquery";
 
 // Write JQuery code here!
+let $latestClickedElem = null;
+
+$(document).on('click', function () {
+  $latestClickedElem?.removeClass('open');
+  $latestClickedElem = null;
+});
+
 const $wrapper = $('.wrapper');
 $wrapper.on('click', '.item', function(e) {
   e.stopPropagation();
 
-  $(this).toggleClass('open').siblings().removeClass('open');
+  $latestClickedElem?.removeClass('open');
+
+  if ($(this)?.get(0) !== $latestClickedElem?.get(0)) {
+    $(this).addClass('open');
+  }
+
+  $latestClickedElem = $(this);
 });
 
-const $items = $wrapper.find('.item');
-
-$(document).on('click', function() {
-  $items.removeClass('open');
-});
+// const $wrapper = $('.wrapper');
+// $wrapper.on('click', '.item', function(e) {
+//   e.stopPropagation();
+//
+//   $(this).toggleClass('open').siblings().removeClass('open');
+// });
+//
+// const $items = $wrapper.find('.item');
+//
+// $(document).on('click', function() {
+//   $items.removeClass('open');
+// });
