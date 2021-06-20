14b7803c9ec0dee8c9e86d6c636c6ebf44d353bf
mhkim <yahma25@gmail.com>
2021-06-20 19:48:24 +0900

    #1 UI 컴포넌트 - Context Menu - 바닐라 JS만 사용하여 구현하기 개선
    app 대신 item의 부모인 wrapper를 이용하요 이벤트 위임을 처리하도록 변경.
    가장 최근에 클릭된 element를 저장하여 class name 'open'을 순회하며 찾는 작업을 수행할 필요 없게 함.
    document에 동일한 타입의 이벤트를 추가하여 가장 최근에 클릭된 element만 제어하게 함.
    불필요하게 context element를 찾지않아도 되게 함.

--- ui-component/context-menu/q1_js/index.js
+++ ui-component/context-menu/q1_js/index.js
@@ -1,22 +1,45 @@
 import './style.css';
 
 // Write Javascript code here!
-const app = document.getElementById('app');
-app.addEventListener('click', (e) => {
-  // context 1개만 출력하기 위해 기존 모든 item, context 초기 상태로 복원
-  Array.from(app.getElementsByClassName('item')).forEach(clickedItem => {
-    clickedItem.classList.remove('open');
-    clickedItem.getElementsByClassName('context')[0].classList.remove('open');
-  });
+let latestClickedElem = null;
 
-  const target = e.target;
-  if (target.classList.contains('item')) {
-    target.classList.add('open');
-
-    const context = target.getElementsByClassName('context')[0];
-    context.classList.add('open');
-
-  } else if (target.classList.contains('context')) {
-    target.classList.remove('open');
+document.addEventListener('click', () => {
+  if (latestClickedElem) {
+    latestClickedElem.classList.remove('open');
+    latestClickedElem = null;
   }
 });
+
+const wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', (e) => {
+  const clickedElem = e.target;
+
+  if (clickedElem.classList.contains('item')) {
+    e.stopPropagation();
+
+    latestClickedElem?.classList.remove('open');
+
+    clickedElem.classList.add('open');
+    latestClickedElem = clickedElem;
+  }
+});
+
+// const app = document.getElementById('app');
+// app.addEventListener('click', (e) => {
+//   // context 1개만 출력하기 위해 기존 모든 item, context 초기 상태로 복원
+//   Array.from(app.getElementsByClassName('item')).forEach(clickedItem => {
+//     clickedItem.classList.remove('open');
+//     clickedItem.getElementsByClassName('context')[0].classList.remove('open');
+//   });
+//
+//   const target = e.target;
+//   if (target.classList.contains('item')) {
+//     target.classList.add('open');
+//
+//     const context = target.getElementsByClassName('context')[0];
+//     context.classList.add('open');
+//
+//   } else if (target.classList.contains('context')) {
+//     target.classList.remove('open');
+//   }
+// });
